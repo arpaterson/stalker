3cf2ea970d716f6fd67b79d02f49ccbfb9ed0bc9
root <root@raspberrypi2-1.(none)>
2015-07-15 17:08:13 +1200

    added try excepts around some stuff, and noted need for disambiuation in zone status bit unpacking

--- speedsensor/radar.py
+++ speedsensor/radar.py
@@ -99,8 +99,9 @@ LOCKED = 1
 FORK_MODE_DISABLED = 0
 FORK_MODE_ENABLED = 1
 
-ZONE_OPPOSITE = 0
-ZONE_SAMEBOTH = 1
+ZONE_CLOSING_OPPOSITE = 0
+ZONE_AWAY_SAME = 1
+ZONE_BOTH = 2
 
 TRANSMITTER_ON = 0
 TRANSMITTER_OFF = 1
@@ -191,213 +192,228 @@ class radar(radardata,radarcommon,radarconfig):
 	 radarconfig.__init__(self)
 
 	def __unpackEnhanced(self,buf):
-	 #unpack an Enhanced Output format sentence
-	 self.report.timereceived = self.received #NO only do this if checksum valid
-	 self.report.valid=0
-	 ######
-	 ###### In fact check the checksum first!
+	 try:
+	  #unpack an Enhanced Output format sentence
+	  self.report.timereceived = self.received #NO only do this if checksum valid
+	  self.report.valid=0
+	  ######
+	  ###### In fact check the checksum first! 
 
-	 #check it is an Enhanced Output format sentence
-	 #0xEF 0xFF 0x02 0x01
-	 # if not buf[0] == 0xEF and buf[1] == 0xFF and buf[2] == 0x02 and buf[3] == 0x01:
-	 # sys.stderr.write("__unpackEnhanced(): Incorrect first four bytes")
-	 # return -1
-	 #check message is correct length (always 21 bytes)
-	 if not len(buf)==21:
-	  sys.stderr.write("__unpackEnhanced(): Incorrect message length")
+	  #check it is an Enhanced Output format sentence
+	  #0xEF 0xFF 0x02 0x01
+	  # if not buf[0] == 0xEF and buf[1] == 0xFF and buf[2] == 0x02 and buf[3] == 0x01:
+	  # sys.stderr.write("__unpackEnhanced(): Incorrect first four bytes")
+	  # return -1
+	  #check message is correct length (always 21 bytes)
+	  if not len(buf)==21:
+	   sys.stderr.write("__unpackEnhanced(): Incorrect message length")
 	 
 
-	 #unpack payload length LSB and MSB
+	  #unpack payload length LSB and MSB
 
-	 #unpack command ID 0x00
+	  #unpack command ID 0x00
 
-	 #unpack antenna number 0x01
+	  #unpack antenna number 0x01
 
-	 #unpack target speed lsb msb
+	  #unpack target speed lsb msb
 
-	 #unpack faster speed lsb msb
+	  #unpack faster speed lsb msb
 
-	 #unpack locked speed lsb msb
+	  #unpack locked speed lsb msb
 
-	 #unpack patrol speed lsb msb
+	  #unpack patrol speed lsb msb
 
-	 #unpack direction byte
+	  #unpack direction byte
 
-	 #unpack status byte
+	  #unpack status byte
 
-	 #unpack configuration byte
+	  #unpack configuration byte
 
-	 #calculate checksum of received bytes lsb msb
+	  #calculate checksum of received bytes lsb msb
 
-	 #compare to received checksum bytes lsb msb
+	  #compare to received checksum bytes lsb msb
 
-	 #only return 0 and make assignments if checksum valid
+	  #only return 0 and make assignments if checksum valid
 
-	 sys.stderr.write("__unpackEnhanced(): Not fully implemented yet")
-	 self.report.valid = 0
-	 return -1
+	  sys.stderr.write("__unpackEnhanced(): Not fully implemented yet")
+	  self.report.valid = 0
+	  return -1
+	 except Exception as e:
+	  repr( e )
+	  return -1
 
 
 	def __unpackS(self,buf):
-	 #unpack an S format sentence
-	 self.report.timereceived = self.received
-	 self.report.valid = 0
+	 try:
+	  #unpack an S format sentence
+	  self.report.timereceived = self.received
+	  self.report.valid = 0
 	
-	 #check it is an S format sentence
-	 msgtype = ord(buf[0])
-	 if msgtype != MSG_S_FORMAT:
-	  if self.verbose > 1:
-	   sys.stderr.write("__unpackS(): Message type was not S format\n")
+	  #check it is an S format sentence
+	  msgtype = ord(buf[0])
+	  if msgtype != MSG_S_FORMAT:
+	   if self.verbose > 1:
+	    sys.stderr.write("__unpackS(): Message type was not S format\n")
+	   return -1
+	  
+	  #unpack  faster target direction
+	  if buf[1] != ' ':
+	   if buf[1] == 'A':
+	    self.report.fasterspeeddir = DIR_AWAY
+	   elif buf[1] == 'C':
+	    self.report.fasterspeeddir = DIR_CLOSING
+	   else:
+	    self.report.fasterspeeddir = NaN	   
+	    sys.stderr.write("__unpackS(): Unexpected character received in fastertargetdir field")
+	  else:
+	   self.report.fasterspeeddir = NaN
+
+	  #unpack  faster target speed	 
+	  if buf[2:6] != '    ':
+	   fasterspeed = int(buf[2:6])
+	   if buf[5] != ' ':
+	    fasterspeed = fasterspeed /10
+	   self.report.fasterspeed = fasterspeed
+	  else:
+	   self.report.fasterspeed = NaN
+
+	  #unpack  strongest target direction
+	  if buf[6] != ' ':
+	   if buf[6] == 'A':
+	    self.report.strongspeeddir = DIR_AWAY
+	   elif buf[6] == 'C':
+	    self.report.strongspeeddir = DIR_CLOSING
+	   else:
+	    self.report.strongspeeddir = NaN	   
+	    sys.stderr.write("__unpackS(): Unexpected character received in strongesttargetdir field\n")
+	  else:
+	   self.report.strongspeeddir = NaN
+
+	  #unpack strongest target speed	 
+	  if buf[7:11] != '    ':
+	   strongspeed = int(buf[7:11])
+	   if buf[10] != ' ':
+	    strongspeed = strongspeed / 10	  
+	   self.report.strongspeed = strongspeed
+	  else:
+	   self.report.strongspeed = NaN
+
+	  #unpack strongest target strength
+	  if buf[11:14] != '   ':
+	   self.report.strongstrength = int(buf[11:14])
+	  else:
+	   self.report.strongstrength = NaN
+
+	  #unpack  channel signal strength ratio
+	  if buf[14:17] != '   ':
+	   self.report.channelsignalstrengthratio = int(buf[14:17])
+	  else:
+	   self.report.channelsignalstrengthratio = NaN
+
+	  #unpack  status byte
+	  statusbyte = ord(buf[17])
+	  if statusbyte & S_SB_forkmode_enabled:
+	   self.report.forkmode = FORK_MODE_ENABLED 
+	  else:
+	   self.report.forkmode = FORK_MODE_DISABLED	 
+	 
+	  self.report.valid = 1
+	  return 0
+
+	 except Exception as  e:
+	  repr( e )
 	  return -1
 	 
-	 #unpack  faster target direction
-	 if buf[1] != ' ':
-	  if buf[1] == 'A':
-	   self.report.fasterspeeddir = DIR_AWAY
-	  elif buf[1] == 'C':
-	   self.report.fasterspeeddir = DIR_CLOSING
-	  else:
-	   self.report.fasterspeeddir = NaN	   
-	   sys.stderr.write("__unpackS(): Unexpected character received in fastertargetdir field")
-	 else:
-	  self.report.fasterspeeddir = NaN
-
-	 #unpack  faster target speed	 
-	 if buf[2:6] != '    ':
-	  fasterspeed = int(buf[2:6])
-	  if buf[5] != ' ':
-	   fasterspeed = fasterspeed /10
-	  self.report.fasterspeed = fasterspeed
-	 else:
-	  self.report.fasterspeed = NaN
-
-	 #unpack  strongest target direction
-	 if buf[6] != ' ':
-	  if buf[6] == 'A':
-	   self.report.strongspeeddir = DIR_AWAY
-	  elif buf[6] == 'C':
-	   self.report.strongspeeddir = DIR_CLOSING
-	  else:
-	   self.report.strongspeeddir = NaN	   
-	   sys.stderr.write("__unpackS(): Unexpected character received in strongesttargetdir field\n")
-	 else:
-	  self.report.strongspeeddir = NaN
-
-	 #unpack strongest target speed	 
-	 if buf[7:11] != '    ':
-	  strongspeed = int(buf[7:11])
-	  if buf[10] != ' ':
-	   strongspeed = strongspeed / 10	  
-	  self.report.strongspeed = strongspeed
-	 else:
-	  self.report.strongspeed = NaN
-
-	 #unpack strongest target strength
-	 if buf[11:14] != '   ':
-	  self.report.strongstrength = int(buf[11:14])
-	 else:
-	  self.report.strongstrength = NaN
-
-	 #unpack  channel signal strength ratio
-	 if buf[14:17] != '   ':
-	  self.report.channelsignalstrengthratio = int(buf[14:17])
-	 else:
-	  self.report.channelsignalstrengthratio = NaN
-
-	 #unpack  status byte
-	 statusbyte = ord(buf[17])
-	 if statusbyte & S_SB_forkmode_enabled:
-	  self.report.forkmode = FORK_MODE_ENABLED 
-	 else:
-	  self.report.forkmode = FORK_MODE_DISABLED	 
-	 
-	 self.report.valid = 1
-	 return 0
 
 
 	def __unpackB(self,buf):
-         # unpack a B format sentence
-         # buf should contain a complete sentence to the carriage return
-         # as per the Stalker Radar Speed Sensor User Manual, A-3.
+	 try:
+          # unpack a B format sentence
+          # buf should contain a complete sentence to the carriage return
+          # as per the Stalker Radar Speed Sensor User Manual, A-3.
 
-	 self.report.timereceived = self.received
-	 self.report.valid = 0
+	  self.report.timereceived = self.received
+	  self.report.valid = 0
 
- 	 #check it is a B format sentence
-	 msgtype = ord(buf[0])
-	 if msgtype != MSG_B_FORMAT:
+ 	  #check it is a B format sentence
+	  msgtype = ord(buf[0])
+	  if msgtype != MSG_B_FORMAT:
+	   if self.verbose > 1:
+	    sys.stderr.write("__unpackB(): Message type was not B format\n")
+	   return -1
+
+	  #unpack status 1 byte	 
+	  status1byte = ord(buf[1])
 	  if self.verbose > 1:
-	   sys.stderr.write("__unpackB(): Message type was not B format\n")
-	  return -1
+	   sys.stderr.write("status1byte arrived at __unpackB()\n  contents: %s\n" % (status1byte))
+	  if status1byte & B_SB1_lockstatus_locked:
+	   self.report.lockstatus = LOCKED
+	  else:
+	   self.report.lockstatus = NOT_LOCKED
 
-	 #unpack status 1 byte	 
-	 status1byte = ord(buf[1])
-	 if self.verbose > 1:
-	  sys.stderr.write("status1byte arrived at __unpackB()\n  contents: %s\n" % (status1byte))
-	 if status1byte & B_SB1_lockstatus_locked:
-	  self.report.lockstatus = LOCKED
-	 else:
-	  self.report.lockstatus = NOT_LOCKED
-
-	 if status1byte & B_SB1_zone_sameboth:
-	  self.report.zone = ZONE_SAMEBOTH
-	 else:
-	  self.report.zone = ZONE_OPPOSITE
+	  if status1byte & B_SB1_zone_sameboth:
+	   self.report.zone = ZONE_AWAY_SAME # or Same/Both ??? Three modes: Closing/Opp, Away/Same, Both. What to do here?
+	  else:
+	   self.report.zone = ZONE_CLOSING_OPPOSITE
 	
-	 if status1byte & B_SB1_forkmode_enabled:
-	  self.report.forkmode = FORK_MODE_ENABLED
-	 else:
-	  self.report.forkmode = FORK_MODE_DISABLED
+	  if status1byte & B_SB1_forkmode_enabled:
+	   self.report.forkmode = FORK_MODE_ENABLED
+	  else:
+	   self.report.forkmode = FORK_MODE_DISABLED
 
-	 if status1byte & B_SB1_transmitter_on:
-	  self.report.transmitterstatus = TRANSMITTER_ON
-	 else:
-	  self.report.transmitterstatus = TRANSMITTER_OFF
+	  if status1byte & B_SB1_transmitter_on:
+	   self.report.transmitterstatus = TRANSMITTER_ON
+	  else:
+	   self.report.transmitterstatus = TRANSMITTER_OFF
 
-	 #unpack status 2 byte
-	 status2byte = ord(buf[2])
-	 if status2byte & B_SB2_fastlockstatus_locked:
-	  self.report.fastlockstatus = FASTLOCKED
-	 else:
-	  self.report.fastlockstatus = NOT_FASTLOCKED
+	  #unpack status 2 byte
+	  status2byte = ord(buf[2])
+	  if status2byte & B_SB2_fastlockstatus_locked:
+	   self.report.fastlockstatus = FASTLOCKED
+	  else:
+	   self.report.fastlockstatus = NOT_FASTLOCKED
 
-	 if status2byte & B_SB2_fastlock_enabled:
-	  self.report.fastlockmode = FASTER_ENABLED
-	 else:
-	  self.report.fastlockmode = FASTER_DISABLED
+	  if status2byte & B_SB2_fastlock_enabled:
+	   self.report.fastlockmode = FASTER_ENABLED
+	  else:
+	   self.report.fastlockmode = FASTER_DISABLED
 	 
-	 if status2byte & B_SB2_voltage_low:
-	  self.report.voltagestatus = VOLTAGE_LOW
-	 else:
-	  self.report.voltagestatus = VOLTAGE_NORMAL
+	  if status2byte & B_SB2_voltage_low:
+	   self.report.voltagestatus = VOLTAGE_LOW
+	  else:
+	   self.report.voltagestatus = VOLTAGE_NORMAL
 
-	 #speeds returned in ascii, convert to int and copy to data members.
-	 if self.verbose >1 :
-	  sys.stderr.write("unpacking speeds...\n  buf[3:15] contents: %s\n  type: %s\n " % (buf[3:15], type(buf[3:15])) )
-	 if buf[3:6] != '   ':
-	  self.report.patrolspeed = int(buf[3:6])
-	 else:
-	  self.report.patrolspeed = NaN
+	  #speeds returned in ascii, convert to int and copy to data members.
+	  if self.verbose >1 :
+	   sys.stderr.write("unpacking speeds...\n  buf[3:15] contents: %s\n  type: %s\n " % (buf[3:15], type(buf[3:15])) )
+	  if buf[3:6] != '   ':
+	   self.report.patrolspeed = int(buf[3:6])
+	  else:
+	   self.report.patrolspeed = NaN
 	 
-	 if not buf[6:9] == '   ':
-	  self.report.lockedspeed = int(buf[6:9])
-	 else:
-	  self.report.lockedspeed = NaN
+	  if not buf[6:9] == '   ':
+	   self.report.lockedspeed = int(buf[6:9])
+	  else:
+	   self.report.lockedspeed = NaN
 
-	 if not buf[9:12] == '   ':
-	  self.report.fasterspeed = int(buf[9:12])
-	 else:
-	  self.report.fasterspeed = NaN
+	  if not buf[9:12] == '   ':
+	   self.report.fasterspeed = int(buf[9:12])
+	  else:
+	   self.report.fasterspeed = NaN
 
-	 if not buf[12:15] == '   ':
-	  self.report.targetspeed = int(buf[12:15])
-	 else:
-	  self.report.targetspeed = NaN
+	  if not buf[12:15] == '   ':
+	   self.report.targetspeed = int(buf[12:15])
+	  else:
+	   self.report.targetspeed = NaN
 
-	 if self.verbose > 1:
-	  sys.stderr.write( "Reached end of __unpackB()")
-	 self.report.valid = 1
-	 return 0
+	  if self.verbose > 1:
+	   sys.stderr.write( "Reached end of __unpackB()")
+	  self.report.valid = 1
+	  return 0
+
+	 except Exception as e:
+	  repr( e )
+	  return -1
 
 	def read(self):
 	 #Read and interpret data
@@ -417,12 +433,13 @@ class radar(radardata,radarcommon,radarconfig):
 	  if self.verbose >1:
 	   sys.stderr.write("Failed to identify self.response in radar.read()\nContents of self.response: %s" % (self.response) )
 	  #some exception stuff here?
-	 return 0
+	 return -1
 
 
 	def next(self):
 	 if self.read() == -1:
-	  raise StopIteration
+	  pass
+	  #raise StopIteration 
 	 return self.response
 
 	def close(self):
